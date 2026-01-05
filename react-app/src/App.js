900379e6820e312406118bfc78939854e9aa8995
Muhammed Aslam Shah S <muhammedaslamshahsofficial@gmail.com>
2026-01-05 01:49:01 -0800

    2nd commit : {Updated file App.js for Memorization logic build}

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,15 +3,35 @@ import React, { useState, useCallback, useMemo, Suspense, lazy } from "react";
 const Counter = lazy(() => import("./Counter"));
 
 const App = () => {
-    const [counter, setCount] = useState(0);
-    const increment = useCallback(() => setCount((currentCounter) => currentCounter + 1), [setCount]);
+    // const [counter, setCount] = useState(0);
+    // const increment = useCallback(() => setCount((currentCounter) => currentCounter + 1), [setCount]);
+
+    // memorization
+    let count = 0;
+    let memo = [];
+    function displayData() {
+        if (memo[count] === undefined) {
+            for (let i = 0; i < 1000000000; i++) {
+                count = i;
+            }
+            memo[count] = count;
+            return count;
+        } else {
+            return memo[count];
+        }
+    }
+
+    console.time();
+    console.log(displayData());
+    console.log(displayData());
+    console.timeEnd();
     return (
         <div>
-            <h1>{counter}</h1>
+            {/* <h1>{counter}</h1>
             <button onClick={increment}>Increment</button>
             <Suspense fallback={<h1>Loading...</h1>}>
                 <Counter />
-            </Suspense>
+            </Suspense> */}
         </div>
     );
 };
